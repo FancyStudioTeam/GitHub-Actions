f91d4df4ad63248ffd3d887480d40393563bb7f6
J.J. Sosa L. <251519186+jjsosal@users.noreply.github.com>
2026-03-19 03:28:05 +0100

    chore: things

--- src/utils/markdown/formatRepositoryHyperlink.ts
+++
@@ -1,5 +0,0 @@
-import { escapeInlineCode, hyperlink, inlineCode } from '@discordjs/formatters';
-
-export function formatRepositoryHyperlink(repositoryFullName: string, repositoryUrl: string) {
-	return hyperlink(inlineCode(escapeInlineCode(repositoryFullName)), repositoryUrl);
-}
