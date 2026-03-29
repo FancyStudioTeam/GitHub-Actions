67bc58fb79511ccb21faab3bdabd918ea83398ec
JJSosaL <hello@fancystudio.xyz>
2026-03-29 22:53:27 +0200

    feat: add pull request opened event (#15)

---
+++ src/events/pull_request/PullRequestOpened.ts
@@ -0,0 +1,85 @@
+import {
+	bold,
+	ContainerBuilder,
+	escapeMarkdown,
+	HeadingLevel,
+	heading,
+	hyperlink,
+	TextDisplayBuilder,
+} from '@discordjs/builders';
+import type { PullRequestOpenedEvent } from '@octokit/webhooks-types';
+import { Colors } from '#/lib/Colors.js';
+
+export const PullRequestOpenedEventHandler = {
+	createContainerBuilder(): ContainerBuilder {
+		return new ContainerBuilder();
+	},
+
+	createContainerSubtitleBuilder(
+		pullRequestOpenedEvent: PullRequestOpenedEvent,
+	): TextDisplayBuilder {
+		const subtitleString = this.formatContainerSubtitle(pullRequestOpenedEvent);
+		const subtitleBuilder = this.createTextDisplayBuilder();
+
+		subtitleBuilder.setContent(subtitleString);
+
+		return subtitleBuilder;
+	},
+
+	createContainerTitleBuilder(
+		pullRequestOpenedEvent: PullRequestOpenedEvent,
+	): TextDisplayBuilder {
+		const titleString = this.formatContainerTitle(pullRequestOpenedEvent);
+		const titleBuilder = this.createTextDisplayBuilder();
+
+		titleBuilder.setContent(titleString);
+
+		return titleBuilder;
+	},
+
+	createEventContainerBuilder(pullRequestOpenedEvent: PullRequestOpenedEvent): ContainerBuilder {
+		const containerBuilder = this.createContainerBuilder();
+
+		const containerSubtitleBuilder =
+			this.createContainerSubtitleBuilder(pullRequestOpenedEvent);
+		const containerTitleBuilder = this.createContainerTitleBuilder(pullRequestOpenedEvent);
+
+		containerBuilder.setAccentColor(Colors.Emerald);
+		containerBuilder.addTextDisplayComponents(containerTitleBuilder, containerSubtitleBuilder);
+
+		return containerBuilder;
+	},
+
+	createTextDisplayBuilder(): TextDisplayBuilder {
+		return new TextDisplayBuilder();
+	},
+
+	formatContainerSubtitle({ pull_request }: PullRequestOpenedEvent): string {
+		const {
+			html_url: pullRequestHtmlUrl,
+			number: pullRequestNumber,
+			title: pullRequestTitle,
+			user: pullRequestUser,
+		} = pull_request;
+		const { login: pullRequestUserLogin } = pullRequestUser;
+
+		const formattedTitle = hyperlink(
+			escapeMarkdown(`[#${pullRequestNumber}] ${pullRequestTitle} - ${pullRequestUserLogin}`),
+			pullRequestHtmlUrl,
+		);
+
+		return bold(formattedTitle);
+	},
+
+	formatContainerTitle({ pull_request, repository }: PullRequestOpenedEvent): string {
+		const { html_url: pullRequestHtmlUrl } = pull_request;
+		const { name: repositoryName } = repository;
+
+		const formattedTitle = hyperlink(
+			escapeMarkdown(`[${repositoryName}] Pull Request Opened`),
+			pullRequestHtmlUrl,
+		);
+
+		return heading(formattedTitle, HeadingLevel.Three);
+	},
+} as const;
--- src/utils/createWebhookMessage.ts
+++ src/utils/createWebhookMessage.ts
@@ -1,7 +1,15 @@
 import type { ContainerBuilder } from '@discordjs/builders';
-import type { PushEvent, StarCreatedEvent, StarEvent, WebhookEvent } from '@octokit/webhooks-types';
+import type {
+	PullRequestEvent,
+	PullRequestOpenedEvent,
+	PushEvent,
+	StarCreatedEvent,
+	StarEvent,
+	WebhookEvent,
+} from '@octokit/webhooks-types';
 import { match, P } from 'ts-pattern';
 import { PushEventHandler } from '#/events/Push.js';
+import { PullRequestOpenedEventHandler } from '#/events/pull_request/PullRequestOpened.js';
 import { StarCreatedEventHandler } from '#/events/star/StarCreated.js';
 
 export function createWebhookMessage(
@@ -10,6 +18,19 @@ export function createWebhookMessage(
 ): ContainerBuilder | null {
 	return match(webhookEventName)
 		.returnType<ContainerBuilder | null>()
+		.with('pull_request', () => {
+			const { action } = webhookEvent as PullRequestEvent;
+
+			return match(action)
+				.returnType<ContainerBuilder | null>()
+				.with('opened', () =>
+					PullRequestOpenedEventHandler.createEventContainerBuilder(
+						webhookEvent as PullRequestOpenedEvent,
+					),
+				)
+				.with(P._, () => null)
+				.run();
+		})
 		.with('push', () => PushEventHandler.createEventContainerBuilder(webhookEvent as PushEvent))
 		.with('star', () => {
 			const { action } = webhookEvent as StarEvent;
